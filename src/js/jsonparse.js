4ed53fa06a41a7c409b3c49c7348f4b934c2c91f
yukinaka <yuki.n.0918@gmail.com>
2015-01-19 21:37:18 +0900

    とりあえず画像取得のロジック作成

--- src/js/jsonparse.js
+++ src/js/jsonparse.js
@@ -1,6 +1,9 @@
 (function(window, $, undefined) {
   $(function() {
-    var feedUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/hatena/b/hotentry&num=-1';
+
+    var feedUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/hatena/b/hotentry&num=-1',
+      feedArray = [],
+      $wrapper = $('#jsonparse');
 
     $.ajax({
       url: feedUrl,
@@ -8,10 +11,19 @@
       cache: false,
       dataType: 'jsonp',
       success: function(data) {
-        for (var i = 0; i < data.responseData.feed.entries.length; i++ ) {
-          console.log(data.responseData.feed.entries[i].title);
-        }
+        displayFeed(data.responseData.feed.entries);
       }
     });
+
+    displayFeed = function(feedData) {
+
+
+      for (var i = 0; i < feedData.length; i++) {
+        // 画像取得
+        var entryImg = feedData[i].content.match(/(http:){1}[\S_-]+\.(?:jpg|gif|png)/);
+
+        //$wrapper.html('<img src="'+ entryImg[0] +'" alt="" />');
+      }
+    };
   });
 })(window, jQuery);
