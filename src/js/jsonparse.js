e65b6b7c0c1146bfe8c3b8bf926aaeca84c4c55c
yukinaka <yuki.n.0918@gmail.com>
2015-01-19 22:18:10 +0900

    あとurlだけ

--- src/js/jsonparse.js
+++ src/js/jsonparse.js
@@ -2,7 +2,7 @@
   $(function() {
 
     var feedUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/hatena/b/hotentry&num=-1',
-      feedArray = [],
+      htmlstr = '',
       $wrapper = $('#jsonparse');
 
     $.ajax({
@@ -17,13 +17,25 @@
 
     displayFeed = function(feedData) {
 
-
       for (var i = 0; i < feedData.length; i++) {
-        // 画像取得
-        var entryImg = feedData[i].content.match(/(http:){1}[\S_-]+\.(?:jpg|gif|png)/);
 
-        //$wrapper.html('<img src="'+ entryImg[0] +'" alt="" />');
+        var entryTitle = feedData[i].title, // タイトル取得
+            entryImg = feedData[i].content.match(/(http:){1}[\S_-]+\.(?:jpg|gif|png)/); //画像取得
+            entryDate = new Date(feedData[i].publishedDate);
+
+          if( entryDate !== null) {
+            strDate = (entryDate.getMonth() + 1) + '月' + entryDate.getDate() + '日';
+          }
+
+        htmlstr +=
+          '<li>' +
+          '<span class="date">' + strDate + '</span>' +
+          '<h2 clas="title">' + entryTitle + '</h2>' +
+          '<img src="' + entryImg[0] + '" alt="" />' +
+          '</li>';
       }
+
+      $wrapper.html('<ul>' + htmlstr + '</ul>');
     };
   });
 })(window, jQuery);
